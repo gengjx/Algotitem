7379c5d1bd52ac40706df1adf9fdbc38a719a3fd
gjx <ggengjx@163.com>
2021-01-21 15:39:00 +0800

    N皇后问题

--- src/并查集/LeetCode_1202.java
+++ src/并查集/LeetCode_1202.java
@@ -2,7 +2,7 @@ package 并查集;
 
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @Auther: gjx
@@ -35,20 +35,73 @@ import java.util.List;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/smallest-string-with-swaps
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+   "udyyek"
+   [[3,3],[3,0],[5,1],[3,1],[3,4],[3,5]]
  */
 public class LeetCode_1202 {
-
     int [] parents ;
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
 
-        String result =s ;
+        String result ="" ;
 
         init(s.length());
+        //System.out.println(Arrays.toString(parents));
         for (List<Integer> temp  : pairs ){
-            union(temp.get(0),temp.get(1));
+            if (temp.get(0) != temp.get(1)){
+                    union(temp.get(0),temp.get(1));
+
+            }
+
+
+
+        }
+        //System.out.println(Arrays.toString(parents));
+
+        String [] results =new String[s.length()];
+        //获取连通集合
+        Map<Integer, List<Integer> > map =new HashMap<>();
+        for (int i = 0; i <parents.length ; i++) {
+            results[i] = s.charAt(i)+"";
+            if (parents[i] == i){
+                List list =new ArrayList();
+                list.add(i);
+                map.put(i,list);
+            }
+            else{
+                int parent  = findParent(i);
+                ArrayList<Integer> lists =new ArrayList();
+                lists.add(parent);
+                List<Integer> list = map.getOrDefault(parent,lists);
+                list.add(list.size(),i);
+            }
+
+
+        }
+        //System.out.println(map);
+
+
+        //调整位置
+        for (List<Integer> list:map.values()){
+            List<String> list1 = new ArrayList<>();
+            for (int i = 0; i < list.size(); i++) {
+                int index = list.get(i);
+                char c =s.charAt(index);
+
+                list1.add(c+"");
+            }
+            //System.out.println(list1);
+            Collections.sort(list1);
+            for (int i = 0; i < list1.size(); i++) {
+                results[list.get(i)] =list1.get(i);
+            }
         }
 
-        //获取连通集合
+        //System.out.println(Arrays.toString(results));
+
+        for (int i = 0; i < results.length; i++) {
+            result+=results[i];
+        }
+
 
 
 
@@ -62,7 +115,13 @@ public class LeetCode_1202 {
         int rightParent =findParent(right);
 
         if (leftParent != rightParent){
-            parents[rightParent] = leftParent;
+            if (leftParent<rightParent){
+                parents[rightParent] = leftParent;
+            }
+            else {
+                parents[leftParent] = rightParent;
+            }
+
         }
     }
 
@@ -80,6 +139,41 @@ public class LeetCode_1202 {
         for (int i = 0; i <parents.length ; i++) {
             parents[i] = i ;
         }
+    }
+
+    @Test
+    public void test(){
+        List<List<Integer>> pairs =new ArrayList<>();
+        List<Integer> list =new ArrayList<>();
+        list.add(3);
+        list.add(3);
+        pairs.add(new ArrayList<>(list));
+        list.clear();
+        list.add(3);
+        list.add(0);
+        pairs.add(new ArrayList<>(list));
+        list.clear();
+        list.add(5);
+        list.add(1);
+        pairs.add(new ArrayList<>(list));
+        list.clear();
+        list.add(3);
+        list.add(1);
+        pairs.add(new ArrayList<>(list));
+        list.clear();
+        list.add(3);
+        list.add(4);
+        pairs.add(new ArrayList<>(list));
+        list.clear();
+        list.add(3);
+        list.add(5);
+        pairs.add(new ArrayList<>(list));
+        System.out.println(pairs);
+        String s ="udyyek";
+        String result =smallestStringWithSwaps(s,pairs);
+        System.out.println(result);
+
+
     }
 
 
